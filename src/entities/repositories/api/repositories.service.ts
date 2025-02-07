43eef929a4fff9573be46dcb339f3e12bb4ed165
shamex <shamexsx@gmail.com>
2025-02-07 14:59:49 +0300

    fix

--- src/entities/repositories/api/repositories.service.ts
+++ src/entities/repositories/api/repositories.service.ts
@@ -8,7 +8,7 @@ class RepositoriesService {
     page: number = 1
   ): Promise<IRepository[]> {
     const response = await fetch(
-      `${this.BASE_URL}/users/${name}/repos?per_page=20&page=${page}`
+      `${this.BASE_URL}/users/${name}/repos?per_page=20&page=${page}&sort=pushed`
     );
 
     if (!response.ok) {
